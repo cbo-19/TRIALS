f632d5e88d93c736a4a153e417eeb5223a645055
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-08 15:26:20 +0000

    Starting to code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-{#include<stdio.h>
+#include<stdio.h>
 
-int main()
+int main(void)
 {  
 	printf("with proper grammar, but the outcome is a piece of heart\n");
 	return(0);
 }
-}
+
